12dae8c8fbb696418143f6460771c12f9473463f
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-11-05 12:26:34 -0300

    transfer service

--- src/main/java/service/TransferService.java
+++ src/main/java/service/TransferService.java
@@ -1,7 +1,12 @@
 package service;
 
-import org.springframework.stereotype.Service;
+import java.math.BigDecimal;
+import java.sql.SQLException;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import models.Account;
 import repositories.AccountRepository;
 
 @Service
@@ -13,5 +18,18 @@ public class TransferService {
 		this.repo = repo;
 	}
 	
-	
+	@Transactional(rollbackFor=SQLException.class)
+	public void transferMoney(long idSender, long idReceiver, BigDecimal amount) {
+		
+		Account sender = repo.findAccountById(idSender);
+		
+		Account receiver = repo.findAccountById(idReceiver);
+		
+		BigDecimal senderNewAmount = sender.getAmount().subtract(amount);
+		BigDecimal receiverNewAmount = receiver.getAmount().add(amount);
+		
+		repo.changeAmount(idSender, senderNewAmount);
+		repo.changeAmount(idReceiver, receiverNewAmount);
+		
+	}
 }
