5ad1d919a59c36ad89e0307ecd9491a1dfa09876
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-11-05 12:39:51 -0300

    main

--- src/main/java/com/example/demo/SpilcaTransactionsApplication.java
+++ src/main/java/com/example/demo/SpilcaTransactionsApplication.java
@@ -2,8 +2,10 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages={"controllers","repositories","service"})
 public class SpilcaTransactionsApplication {
 
 	public static void main(String[] args) {
